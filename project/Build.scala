c266417d3aed04a485c53ff94c0a8fc17105c016
y-yoshinoya <y-yoshinoya@ase.co.jp>
2014-09-11 10:33:14 +0900

    bump version for next release

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,7 @@ import sbt._
 import Keys._
 
 object ActiveRecordBuild extends Build {
-  val _version = "0.3.0"
+  val _version = "0.3.1"
   val isRelease = System.getProperty("release") == "true"
 
   def specs2(scope: String) = Def.setting {
