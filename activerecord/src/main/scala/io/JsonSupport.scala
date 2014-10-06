7ad07232bd60cdb735ef78e9655e6920df43da8b
y-yoshinoya <y-yoshinoya@ase.co.jp>
2014-10-06 17:37:50 +0900

    #6 add JsonSupport methods

--- activerecord/src/main/scala/io/JsonSupport.scala
+++ activerecord/src/main/scala/io/JsonSupport.scala
@@ -11,6 +11,18 @@ import org.json4s.native.JsonMethods
 import org.json4s.native.Serialization
 
 trait JsonSerializer { self: IO =>
+  def asJson: JValue = {
+    implicit val format =  DefaultFormats
+    Extraction.decompose(toMap)
+  }
+
+  def asJson(onlyFields: List[String]): JValue = {
+    implicit val format =  DefaultFormats
+    Extraction.decompose(toMap(onlyFields))
+  }
+
+  def asJson(onlyFields: String*): JValue = asJson(onlyFields.toList)
+
   def toJson: String = {
     implicit val format =  DefaultFormats
     Serialization.write(toMap)
@@ -23,22 +35,34 @@ trait JsonSerializer { self: IO =>
 
   def toJson(onlyFields: String*): String = toJson(onlyFields.toList)
 
-  def fromJson(json: String): self.type = {
-    implicit val format =  DefaultFormats
-    assignFormValues(JsonMethods.parse(json).values.asInstanceOf[Map[String, Any]].mapValues(_.toString))
-  }
+  def fromJson(json: String): self.type = fromJValue(JsonMethods.parse(json))
+
+  def fromJValue(jvalue: JValue): self.type =
+    assignFormValues(jvalue.values.asInstanceOf[Map[String, Any]].mapValues(_.toString))
 }
 
 trait JsonSupport[T <: ActiveModel] { self: FormSupport[T] =>
-  def fromJson(json: String) = {
-    implicit val format =  DefaultFormats
-    bind(JsonMethods.parse(json).values.asInstanceOf[Map[String, Any]].mapValues(_.toString))
-  }
+  def fromJson(json: String): T = fromJValue(JsonMethods.parse(json))
+
+  def fromArrayJson(json: String): List[T] = fromJArray(JsonMethods.parse(json))
+
+  def fromJValue(jvalue: JValue): T =
+    bind(jvalue.values.asInstanceOf[Map[String, Any]].mapValues(_.toString))
+
+  def fromJArray(jarray: JValue): List[T] =
+    jarray.values.asInstanceOf[List[Map[String, Any]]].map(m => bind(m.mapValues(_.toString)))
 }
 
 trait JsonImplicits { self: DSL =>
-  implicit class IterableJsonSerializer[T <: AR : ClassTag](list: Iterable[T]) {
+
+  class IterableJsonSerializer[T <: AR : ClassTag](list: Iterable[T]) {
     private implicit val format =  DefaultFormats
+
+    def asJson: JValue = Extraction.decompose(list.map(_.toMap))
+
+    def asJson(onlyFields: List[String]): JValue = Extraction.decompose(list.map(_.toMap(onlyFields)))
+
+    def asJson(onlyFields: String*): JValue = asJson(onlyFields.toList)
 
     def toJson: String = Serialization.write(list.map(_.toMap))
 
@@ -48,14 +72,20 @@ trait JsonImplicits { self: DSL =>
     def toJson(onlyFields: String*): String = toJson(onlyFields.toList)
   }
 
+  implicit def toJsonable[T <: AR : ClassTag](list: Iterable[T]) =
+    new IterableJsonSerializer(list)
+
   // for Scala 2.11+ only
-  implicit class IterableLikeJsonSerializer[T <: AR: ClassTag, ILike <% Iterable[T]](list: ILike) {
+  implicit def toJsonable[T <: AR: ClassTag, ILike <% Iterable[T]](list: ILike) =
+    new IterableJsonSerializer(list)
+
+  implicit class GroupByJsonable[T <: AR: ClassTag](grouped: Map[_, List[T]]) {
     private implicit val format =  DefaultFormats
 
-    def toJson: String = Serialization.write(list.map(_.toMap))
+    def toJson: String = Serialization.write(grouped.map{ case (k, v) => (k.toString, v.asJson) })
 
     def toJson(onlyFields: List[String]): String =
-      Serialization.write(list.map(_.toMap(onlyFields)))
+      Serialization.write(grouped.map{ case (k, v) => (k.toString, v.asJson(onlyFields)) })
 
     def toJson(onlyFields: String*): String = toJson(onlyFields.toList)
   }
--- activerecord/src/test/scala/io/JsonSupportSpec.scala
+++ activerecord/src/test/scala/io/JsonSupportSpec.scala
@@ -3,9 +3,19 @@ package com.github.aselab.activerecord.io
 import com.github.aselab.activerecord._
 import dsl._
 import models._
+import org.json4s._
 
 object JsonSupportSpec extends DatabaseSpecification {
   "JsonSerializer " should {
+    "asJson" >> {
+      User("foo", true).asJson mustEqual JObject(List(JField("name",JString("foo")), JField("isAdmin",JBool(true))))
+    }
+
+    "asJson(parameters)" >> {
+      User("foo", true).asJson("name") mustEqual JObject(List(JField("name",JString("foo"))))
+      User("foo", true).asJson("name", "id", "isNewRecord") mustEqual JObject(List(("name",JString("foo")), ("id",JInt(0)), ("isNewRecord",JBool(true))))
+    }
+
     "toJson" >> {
       User("foo", true).toJson mustEqual """{"name":"foo","isAdmin":true}"""
     }
@@ -24,9 +34,42 @@ object JsonSupportSpec extends DatabaseSpecification {
     "fromJson" >> {
       User.fromJson("""{"name":"foo","isAdmin":true}""") mustEqual User("foo", true)
     }
+
+    "fromArrayJson" >> {
+      val json = """[{"name":"foo","isAdmin":true},{"name":"bar","isAdmin":false}]"""
+      User.fromArrayJson(json) mustEqual List(User("foo", true), User("bar", false))
+    }
+
+    "fromJValue" >> {
+      val jvalue = JObject(List(JField("name",JString("foo")), JField("isAdmin",JBool(true))))
+      User.fromJValue(jvalue) mustEqual User("foo", true)
+    }
+
+    "fromJArray" >> {
+      val jarray = JArray(List(
+        JObject(List(JField("name",JString("foo")), JField("isAdmin",JBool(true)))),
+        JObject(List(JField("name",JString("bar")), JField("isAdmin",JBool(false))))
+      ))
+
+      User.fromJArray(jarray) mustEqual List(User("foo", true), User("bar", false))
+    }
   }
 
   "JsonImplicits" should {
+    "asJson" >> {
+      List(User("foo"), User("bar", true)).asJson mustEqual JArray(List(
+        JObject(List(JField("name",JString("foo")), JField("isAdmin",JBool(false)))),
+        JObject(List(JField("name",JString("bar")), JField("isAdmin",JBool(true))))
+      ))
+    }
+
+    "asJson(parameters)" >> {
+      List(User("foo"), User("bar", true)).asJson("name", "isNewRecord") mustEqual JArray(List(
+        JObject(List(JField("name",JString("foo")), JField("isNewRecord",JBool(true)))),
+        JObject(List(JField("name",JString("bar")), JField("isNewRecord",JBool(true))))
+      ))
+    }
+
     "toJson" >> {
       User("foo").save
       User("bar", true).save
@@ -36,6 +79,16 @@ object JsonSupportSpec extends DatabaseSpecification {
     "toJson(parameters)" >> {
       TestTables.createTestData
       PrimitiveModel.where(_.id.~ < 3).toList.toJson("string", "oint") mustEqual """[{"string":"string1","oint":1},{"string":"string2","oint":2}]"""
+    }
+
+    "toJson(groupBy)" >> {
+      List(User("foo"), User("bar", true), User("hoge", true)).groupBy(_.isAdmin).toJson mustEqual
+        """{"false":[{"name":"foo","isAdmin":false}],"true":[{"name":"bar","isAdmin":true},{"name":"hoge","isAdmin":true}]}"""
+    }
+
+    "toJson(groupBy, parameters)" >> {
+      List(User("foo"), User("bar", true), User("hoge", true)).groupBy(_.isAdmin).toJson("name") mustEqual
+        """{"false":[{"name":"foo"}],"true":[{"name":"bar"},{"name":"hoge"}]}"""
     }
   }
 }
