0bd774ed0375431ec944061ce59949a828ac4e03
y-yoshinoya <y-yoshinoya@ase.co.jp>
2016-08-16 11:03:45 +0900

    fix deprecated warnings

--- macro/src/main/scala/Deprecations.scala
+++ macro/src/main/scala/Deprecations.scala
@@ -4,7 +4,7 @@ import scala.language.experimental.macros
 import scala.reflect.macros._
 
 trait Deprecations {
-  def unsupportedInTransaction[A](c: Context)(a: c.Expr[A]): c.Expr[A] = {
+  def unsupportedInTransaction[A](c: whitebox.Context)(a: c.Expr[A]): c.Expr[A] = {
     import c.universe._
     c.error(c.enclosingPosition, "dsl#inTransaction is deprecated. use ActiveRecordCompanion#inTransaction instead.")
     reify(a.splice)
